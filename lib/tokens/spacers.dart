b577d17097114eec58468760fba284bf2a632031
Martin Chaine <martin@casimir.me>
2024-06-13 19:50:03 +0200

    spacers, add horizontal default

--- lib/tokens/spacers.dart
+++ lib/tokens/spacers.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 
 class Spacers {
+  final Widget horizontalContent = const SizedBox(width: 16.0);
   final Widget verticalContent = const SizedBox(height: 16.0);
 }
