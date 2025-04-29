0b0d93eaf8d5c1dd27b48a1ff8e1d91252ecdda9
Martin Chaine <martin@casimir.me>
2025-04-29 13:20:13 +0200

    feat(icons): add more icons

--- lib/tokens/adaptive/icons.dart
+++ lib/tokens/adaptive/icons.dart
@@ -10,8 +10,11 @@ class AdaptiveIconFactory {
       _platform == TargetPlatform.iOS || _platform == TargetPlatform.macOS;
 
   IconData get delete => _isApple ? CupertinoIcons.delete : Icons.delete;
+  IconData get done => _isApple ? CupertinoIcons.checkmark : Icons.done;
   IconData get info => _isApple ? CupertinoIcons.info : Icons.info_outline;
   IconData get settings =>
       _isApple ? CupertinoIcons.settings : Icons.settings_outlined;
   IconData get share => _isApple ? CupertinoIcons.share : Icons.share;
+  IconData get star => _isApple ? CupertinoIcons.star : Icons.star_border;
+  IconData get starFilled => _isApple ? CupertinoIcons.star_fill : Icons.star;
 }
