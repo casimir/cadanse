e4a0d3b0250727131034419d6f778a4ec90d398b
Martin Chaine <martin@casimir.me>
2025-04-29 13:52:32 +0200

    feat(TargetType): enforce `material` when in a web browser (+ some doc)

--- lib/shared/target_type.dart
+++ lib/shared/target_type.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/foundation.dart';
 
+/// The type of target (Design System) the interface should adapt to.
 enum TargetType {
+  /// Dynamically determined based on [TargetPlatform].
   adaptive,
+
+  /// Human Guidelines Interface: <https://developer.apple.com/design/human-interface-guidelines/>
   human,
+
+  /// Material Design: <https://m3.material.io/>
   material;
 
+  /// Returns the effective target type based on the current platform.
+  ///
+  /// When running in a browser, [material] is enforced regardless of [platform].
   TargetType effectiveType(TargetPlatform platform) {
     if (this != adaptive) return this;
+
+    if (kIsWeb) return material;
 
     return platform == TargetPlatform.iOS || platform == TargetPlatform.macOS
         ? human
